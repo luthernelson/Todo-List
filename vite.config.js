6019b04454208a83588621b463b1c3854a821347
Luther Nelson <luthernelson732@gmail.com>
2025-02-17 11:50:41 +0100

    version1.1

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 2000, // Limite en Ko (2000 Ko = 2 Mo)
   },
 })
